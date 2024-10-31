a10045cff1229a2028f85c857d3b0d0d5db55a84
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-10-31 17:05:00 +0100

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,8 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-
 nameInput.addEventListener('input', () => {
-
+ 
   const inputValue = nameInput.value.trim();
 
   
