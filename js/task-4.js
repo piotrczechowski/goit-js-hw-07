d7886bfeb21674d99d1ef6e4e00feefff841c868
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-10-31 17:15:06 +0100

    fix 2

--- js/task-4.js
+++ js/task-4.js
@@ -5,12 +5,12 @@ loginForm.addEventListener('submit', (event) => {
 
   event.preventDefault();
 
-  const emailInput = loginForm.elements['email'];
-  const passwordInput = loginForm.elements['password'];
+//   const emailInput = loginForm.elements['email'];
+//   const passwordInput = loginForm.elements['password'];
+const { email, password } = loginForm.elements;
 
-
-  const emailValue = emailInput.value.trim();
-  const passwordValue = passwordInput.value.trim();
+//   const emailValue = emailInput.value.trim();
+//   const passwordValue = passwordInput.value.trim();
 
   if (!emailValue || !passwordValue) {
     alert('All form fields must be filled in.');
